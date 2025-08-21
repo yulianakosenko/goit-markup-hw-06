5eea0ce69645c0f1310ca8336bfc858d1dca2b88
Yuliya Kostenko <yuliashkodina1@gmail.com>
2025-08-21 16:46:15 +0200

    3 21.08

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+    console.log('Menu toggled');
+  }
+})();
